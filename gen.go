b3dc37d575df33dedd3b6e2dc4e47898a7b5cefc
Adam Olsen <arolsen@gmail.com>
2024-09-06 16:29:28 -0600

    go generate and cobra

---
+++ gen.go
@@ -0,0 +1,4 @@
+package main
+
+//go:generate templ generate
+//go:generate ./node_modules/.bin/tailwindcss --postcss -i ./assets/css/main.css -o ./assets/css/compiled/main.css --minify
--- internal/route/default.go
+++ internal/route/default.go
@@ -5,21 +5,19 @@ import (
 
 	"github.com/a-h/templ"
 
-	"github.com/synic/adamthings.me/internal/config"
 	"github.com/synic/adamthings.me/internal/route/component"
 )
 
 type defaultRouter struct {
 	assetsDir string
-	conf      config.SiteConfig
 }
 
-func NewDefaultRouter(conf config.SiteConfig, assetsDir string) defaultRouter {
-	return defaultRouter{conf: conf, assetsDir: assetsDir}
+func NewDefaultRouter(assetsDir string) defaultRouter {
+	return defaultRouter{assetsDir: assetsDir}
 }
 
 func (h defaultRouter) Mount(server *http.ServeMux) {
 	fs := http.FileServer(http.Dir(h.assetsDir))
 	server.Handle("GET /static/", http.StripPrefix("/static/", fs))
-	server.Handle("GET /about/{$}", templ.Handler(component.AboutView(h.conf)))
+	server.Handle("GET /about/{$}", templ.Handler(component.AboutView()))
 }

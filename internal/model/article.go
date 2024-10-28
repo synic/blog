3da17a3a75a871a4f4005f58037e2b1bcfec5897
Adam Olsen <arolsen@gmail.com>
2024-10-28 08:57:36 -0600

    some org getting ready for comments

--- internal/model/article.go
+++ internal/model/article.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/a-h/templ"
+)
 
 type Article struct {
 	PublishedAt time.Time `json:"published_at"`
@@ -10,4 +15,18 @@ type Article struct {
 	Slug        string    `json:"slug"`
 	Tags        []string  `json:"tags"`
 	IsPublished bool      `json:"is_published"`
+}
+
+func (a *Article) URL() string {
+	return fmt.Sprintf(
+		"/articles/%d-%02d-%02d/%s",
+		a.PublishedAt.Year(),
+		a.PublishedAt.Month(),
+		a.PublishedAt.Day(),
+		a.Slug,
+	)
+}
+
+func (a *Article) SafeURL() templ.SafeURL {
+	return templ.URL(a.URL())
 }
--- internal/web/pagination.go
+++ internal/web/pagination.go
@@ -1,4 +1,4 @@
-package pagination
+package web
 
 import (
 	"github.com/synic/adamthings.me/internal/model"

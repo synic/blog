6bd734aa8a3cdbd747c7f92f889aae27a7dfbeab
Adam Olsen <arolsen@gmail.com>
2024-11-19 12:16:08 -0700

    cache staticfile output for speed

--- internal/view/static_utils.go
+++ internal/view/static_utils.go
@@ -2,44 +2,19 @@ package view
 
 import (
 	"context"
-	"embed"
 	"fmt"
 	"io"
-	"log"
-	"strings"
 
 	"github.com/a-h/templ"
 )
 
-func inlinestatic(files ...string) templ.Component {
+func cachedstaticfiles() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
-		if fs, ok := ctx.Value("staticFS").(embed.FS); ok {
-			for _, file := range files {
-				data, err := fs.ReadFile(fmt.Sprintf("assets/%s", file))
-				wrap := "script"
-
-				if strings.HasSuffix(file, ".css") {
-					wrap = "style"
-				}
-
-				if err != nil {
-					log.Printf("unable to read static file %s: %v", file, err)
-					return err
-				}
-
-				_, err = io.WriteString(
-					w,
-					fmt.Sprintf("<%s>/* %s */\n%s</%s>", wrap, file, data, wrap),
-				)
-
-				if err != nil {
-					return err
-				}
-			}
-
-			return nil
+		if data, ok := ctx.Value("cached-static-files").(*[]byte); ok {
+			_, err := w.Write(*data)
+			return err
 		}
 
-		return fmt.Errorf("unable to locate static files \"%s\"", strings.Join(files, ","))
+		return fmt.Errorf("unable to locate cached static files")
 	})
 }

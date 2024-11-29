d70860e6611ceb5fe9a10c9aa1910f7098cbd2ee
Adam Olsen <arolsen@gmail.com>
2024-11-29 10:23:25 -0700

    fix: fix loadtest script

--- scripts/loadtest.sh
+++ scripts/loadtest.sh
@@ -4,12 +4,7 @@ HOST=${BLOG_HOST:=http://localhost:3000}
 USERS=${NUM_USERS:=100}
 COUNT=${REQUEST_COUNT:=150000}
 
-if ! command -v cassowary &> /dev/null
-then
-    go install github.com/rogerwelin/cassowary/cmd/cassowary@v0.16.0
-fi
-
-cassowary run \
+go run github.com/rogerwelin/cassowary/cmd/cassowary@latest run \
     -u $BLOG_HOST \
     -c $USERS \
     -n $COUNT \
